84e5f1ea17abd385e0e6dce1d4da271ea9645273
Russell_Samatov <russam4515@gmail.com>
2020-12-26 22:57:21 -0500

    project staus view is added

--- src/main/java/com/cybertek/controller/ProjectController.java
+++ src/main/java/com/cybertek/controller/ProjectController.java
@@ -1,8 +1,11 @@
 package com.cybertek.controller;
 
 import com.cybertek.dto.ProjectDTO;
+import com.cybertek.dto.TaskDTO;
+import com.cybertek.dto.UserDTO;
 import com.cybertek.enums.Status;
 import com.cybertek.service.ProjectService;
+import com.cybertek.service.TaskService;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
@@ -20,19 +26,21 @@ public class ProjectController {
     ProjectService projectService;
     @Autowired
     UserService userService;
+    @Autowired
+    TaskService taskService;
 
     @GetMapping("/create")
-    public String createProject(Model model){
+    public String createProject(Model model) {
 
-        model.addAttribute("project",new ProjectDTO());
-        model.addAttribute("projects",projectService.findAll());
-        model.addAttribute("managers",userService.findManagers());
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("managers", userService.findManagers());
 
         return "/project/create";
     }
 
     @PostMapping("/create")
-    public String insertProject(ProjectDTO project){
+    public String insertProject(ProjectDTO project) {
         projectService.save(project);
         project.setProjectStatus(Status.OPEN);
         return "redirect:/project/create";
@@ -40,33 +48,75 @@ public class ProjectController {
     }
 
     @GetMapping("/delete/{projectcode}")
-    public String deleteProject(@PathVariable("projectcode") String projectcode){
+    public String deleteProject(@PathVariable("projectcode") String projectcode) {
         projectService.deleteById(projectcode);
         return "redirect:/project/create";
     }
 
     @GetMapping("/complete/{projectcode}")
-    public String completeProject(@PathVariable("projectcode") String projectcode){
+    public String completeProject(@PathVariable("projectcode") String projectcode) {
         projectService.complete(projectService.findById(projectcode));
         return "redirect:/project/create";
     }
 
     @GetMapping("/update/{projectcode}")
-    public String editProject(@PathVariable("projectcode") String projectcode,Model model){
+    public String editProject(@PathVariable("projectcode") String projectcode, Model model) {
 
-        model.addAttribute("project",projectService.findById(projectcode));
-        model.addAttribute("projects",projectService.findAll());
-        model.addAttribute("managers",userService.findManagers());
+        model.addAttribute("project", projectService.findById(projectcode));
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("managers", userService.findManagers());
 
         return "/project/update";
     }
 
     @PostMapping("/update/{projectcode}")
-    public String updateProject(@PathVariable("projectcode") String projectcode,ProjectDTO project,Model model){
+    public String updateProject(@PathVariable("projectcode") String projectcode, ProjectDTO project, Model model) {
 
         projectService.update(project);
 
         return "redirect:/project/create";
+    }
+
+
+
+    @GetMapping("/manager/complete")
+    public String getProjectByManager(Model model){
+
+        UserDTO manager = userService.findById("john@cybertek.com");
+
+        List<ProjectDTO> projects = getCountedListOfProjectDTO(manager);
+
+        model.addAttribute("projects",projects);
+
+
+        return "/manager/project-status";
+    }
+
+
+
+    // curly braces means that we need  return values, first we need to bring all task list
+    List<ProjectDTO> getCountedListOfProjectDTO(UserDTO manager){
+
+        List<ProjectDTO> list = projectService
+                .findAll()
+                .stream()
+                .filter(x -> x.getAssignedManager().equals(manager))
+                .map(x -> {
+
+                    List<TaskDTO> taskList = taskService.findTaskByManager(manager);
+                    int completeCount = (int) taskList.stream().filter(t -> t.getProject().equals(x) && t.getTaskStatus() == Status.COMPLETE).count();
+                    int inCompleteCount = (int) taskList.stream().filter(t -> t.getProject().equals(x) && t.getTaskStatus() != Status.COMPLETE).count();
+
+                    x.setCompleteTaskCounts(completeCount);
+                    x.setUnfinishedTaskCounts(inCompleteCount);
+
+                    return x;
+
+                }).collect(Collectors.toList());
+
+        return list;
+
+
     }
 
 
